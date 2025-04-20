1be0ed9c9c728fe0d2637ade2fde9910e1aaf4a9
Marcelo G. Carbonera <marcelocarbonera@live.com>
2025-04-20 17:17:19 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/imc_utfpr_t2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.imc_utfpr_t2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:imc_utfpr_t2/bmi_controller.dart';
+import 'package:imc_utfpr_t2/pages/insert_data.dart';
+import 'package:imc_utfpr_t2/pages/result_bmi.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => BMIController(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => InsertData(),
+        '/result': (context) => const ResultBMI()
+      },
+    );
+  }
+}
